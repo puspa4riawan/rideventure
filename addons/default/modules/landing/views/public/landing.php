20a6c712160fc1d9b27211a4bd58a9b058c66c06
Puspa Ariawan <arya.k4m4ndaka@gmail.com>
2020-01-13 14:17:10 +0700

    change menu position

--- addons/default/modules/landing/views/public/landing.php
+++ addons/default/modules/landing/views/public/landing.php
@@ -1,81 +1,63 @@
-<!-- welcome -->    
-    <div id="about" class="welcome">
+<!-- welcome -->  
+    <!-- news -->
+    <div id="news" class="news">   
         <div class="container"> 
-            <div class="welcome-agileinfo">
-                <div class="col-md-6 w3ls_welcome_left"> 
-                    <div class="w3ls_welcome_right1">
-                        <h3 class="agileits-title"><span>A</span>bout Us</h3>
-                        <h6>Lorem ipsum dolor <span> Consectetur </span> sit adipisicing. </h6>
-                        <p>Quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
-                            vel illum qui dolorem eum fugiat quo voluptas nulla pariatur Duis finibus sollicitudin libero, sed tempor diam euismod sed. Sed interdum odio a finibus gravida. Aliquam tincidunt nisl nec urna euismod, ullamcorper scelerisque magna vulputate. .</p>
-                    </div> 
+            <h3 class="agileits-title w3title2"><span>N</span>ews </h3>
+            <div class="news-agileinfo">
+                <div class="col-md-4 news-w3grid">
+                    <img src="{{theme:image_url file='img2.jpg'}}" alt="">
+                    <div class="news-w3grid-info">
+                        <h5><span>10</span> April </h5>
+                        <h4>Donec cursus</h4>
+                        <p>Etiam ex lorem cursus vitae placerat suscipit dapibus tortor sed nec augue</p>
+                        <div class="article-links">
+                            <ul>
+                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
+                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-6 w3ls_welcome_right">   
-                    <div class="agileits_w3layouts_welcome_grid">
-                        <img src="{{theme:image_url file='img1.jpg'}}" alt=" " class="img-responsive" />
-                    </div> 
-                </div> 
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-    </div>
-    <!-- //welcome -->  
-    <!-- slid -->
-    <div class="slid jarallax">
-        <div class="slid-text">
-            <h4>24/7 Customer Service Support</h4>
-            <p>Proin tincidunt sodales faucibus. Curabitur ut metus sed urna dignissim sodales ac a tellus. Sed varius justo tellus, at convallis libero cursus non. In malesuada accumsan felis, a imperdiet arcu blandit sed. Ut id faucibus eros. Fusce sed vulputate dui, non consectetur felis. Etiam id enim sem. Suspendisse commodo tempor magna </p>
-        </div>
-        <div class="clearfix"> </div>
-    </div>
-    <!-- //slid -->
-    <!-- services -->
-    <div id="services" class="services">
-        <div class="container"> 
-            <h3 class="agileits-title w3title2"><span>S</span>ervices</h3>
-            <div class="services-row-agileinfo">
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-heart hi-icon" aria-hidden="true"></span>
-                    <h5>Cum soluta nobis</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
+                <div class="col-md-4 news-w3grid">
+                    <img src="{{theme:image_url file='img3.jpg'}}" alt="">
+                    <div class="news-w3grid-info">
+                        <h5><span>15</span> April </h5>
+                        <h4>Suscipit dapib</h4>
+                        <p>Lorem cursus vitae placerat etiam ex suscipit dapibus tortor sed nec augue</p>
+                        <div class="article-links">
+                            <ul>
+                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
+                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-wrench hi-icon" aria-hidden="true"></span>
-                    <h5>Soluta vum nobis</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
+                <div class="col-md-4 news-w3grid">
+                    <img src="{{theme:image_url file='img4.jpg'}}" alt="">
+                    <div class="news-w3grid-info">
+                        <h5><span>18</span> April </h5>
+                        <h4>Tortor sed</h4>
+                        <p>Etiam ex lorem cursus vitae placerat suscipit dapibus tortor sed nec augue</p>
+                        <div class="article-links">
+                            <ul>
+                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
+                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-bell hi-icon" aria-hidden="true"></span>
-                    <h5>Nobis cum soluta</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
-                </div> 
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-motorcycle hi-icon" aria-hidden="true"></span>
-                    <h5>Cum soluta nobis</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
-                </div>
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-check-square-o hi-icon" aria-hidden="true"></span>
-                    <h5>Soluta vum nobis</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
-                </div>
-                <div class="col-sm-4 col-xs-6 services-w3grid">
-                    <span class="fa fa-gears hi-icon" aria-hidden="true"></span>
-                    <h5>Nobis cum soluta</h5>
-                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
-                </div> 
                 <div class="clearfix"> </div>
             </div> 
-         </div>
+        </div> 
     </div>
-    <!-- //services -->
+
     <!-- specifications -->
     <div id="spec" class="spec jarallax">
         <div class="container"> 
-            <h3 class="agileits-title w3title2"><span>S</span>pecifications</h3>
+            <h3 class="agileits-title w3title2"><span>O</span>ur Product</h3>
             <div class="specw3-agileits">
                 <div class="col-md-6 spec-grids">   
-                    <h4>1. Key Features</h4>
+                    <h4>1. Product 1</h4>
                     <ul>
                         <li>
                             <div class="specf-left">
@@ -134,7 +116,7 @@
                     </ul>
                 </div>  
                 <div class="col-md-6 spec-grids">   
-                    <h4>2. Key Features</h4>
+                    <h4>2. Product 2</h4>
                     <ul>
                         <li>
                             <div class="specf-left">
@@ -197,58 +179,60 @@
         </div>
     </div>          
     <!-- //specifications -->
-    <!-- news -->
-    <div id="news" class="news">   
+    <!-- //news -->   
+    
+    <!-- //welcome -->  
+    <!-- slid -->
+    <div class="slid jarallax">
+        <div class="slid-text">
+            <h4>24/7 Customer Service Support</h4>
+            <p>Proin tincidunt sodales faucibus. Curabitur ut metus sed urna dignissim sodales ac a tellus. Sed varius justo tellus, at convallis libero cursus non. In malesuada accumsan felis, a imperdiet arcu blandit sed. Ut id faucibus eros. Fusce sed vulputate dui, non consectetur felis. Etiam id enim sem. Suspendisse commodo tempor magna </p>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <!-- //slid -->
+    <!-- services -->
+    <div id="services" class="services">
         <div class="container"> 
-            <h3 class="agileits-title w3title2"><span>N</span>ews </h3>
-            <div class="news-agileinfo">
-                <div class="col-md-4 news-w3grid">
-                    <img src="{{theme:image_url file='img2.jpg'}}" alt="">
-                    <div class="news-w3grid-info">
-                        <h5><span>10</span> April </h5>
-                        <h4>Donec cursus</h4>
-                        <p>Etiam ex lorem cursus vitae placerat suscipit dapibus tortor sed nec augue</p>
-                        <div class="article-links">
-                            <ul>
-                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
+            <h3 class="agileits-title w3title2"><span>S</span>ervices</h3>
+            <div class="services-row-agileinfo">
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-heart hi-icon" aria-hidden="true"></span>
+                    <h5>Cum soluta nobis</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
                 </div>
-                <div class="col-md-4 news-w3grid">
-                    <img src="{{theme:image_url file='img3.jpg'}}" alt="">
-                    <div class="news-w3grid-info">
-                        <h5><span>15</span> April </h5>
-                        <h4>Suscipit dapib</h4>
-                        <p>Lorem cursus vitae placerat etiam ex suscipit dapibus tortor sed nec augue</p>
-                        <div class="article-links">
-                            <ul>
-                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-wrench hi-icon" aria-hidden="true"></span>
+                    <h5>Soluta vum nobis</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
                 </div>
-                <div class="col-md-4 news-w3grid">
-                    <img src="{{theme:image_url file='img4.jpg'}}" alt="">
-                    <div class="news-w3grid-info">
-                        <h5><span>18</span> April </h5>
-                        <h4>Tortor sed</h4>
-                        <p>Etiam ex lorem cursus vitae placerat suscipit dapibus tortor sed nec augue</p>
-                        <div class="article-links">
-                            <ul>
-                                <li><a href="#"><i class="glyphicon glyphicon-heart-empty"></i><span>1,052</span></a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span>10K</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-bell hi-icon" aria-hidden="true"></span>
+                    <h5>Nobis cum soluta</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
+                </div> 
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-motorcycle hi-icon" aria-hidden="true"></span>
+                    <h5>Cum soluta nobis</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
                 </div>
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-check-square-o hi-icon" aria-hidden="true"></span>
+                    <h5>Soluta vum nobis</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
+                </div>
+                <div class="col-sm-4 col-xs-6 services-w3grid">
+                    <span class="fa fa-gears hi-icon" aria-hidden="true"></span>
+                    <h5>Nobis cum soluta</h5>
+                    <p>Itaque earum rerum hic a sapiente delectus finibus gravida</p>
+                </div> 
                 <div class="clearfix"> </div>
             </div> 
-        </div> 
+         </div>
     </div>
-    <!-- //news --> 
+    <!-- //services -->
+    
+    
     <!-- gallery -->
     <div class="gallery" id="gallery">   
         <h3 class="agileits-title w3title2"><span>G</span>allery</h3>
@@ -321,6 +305,27 @@
         </div>
     </div>
     <!-- //gallery -->
+
+    <div id="about" class="welcome">
+        <div class="container"> 
+            <div class="welcome-agileinfo">
+                <div class="col-md-6 w3ls_welcome_left"> 
+                    <div class="w3ls_welcome_right1">
+                        <h3 class="agileits-title"><span>A</span>bout Us</h3>
+                        <h6>Lorem ipsum dolor <span> Consectetur </span> sit adipisicing. </h6>
+                        <p>Quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
+                            vel illum qui dolorem eum fugiat quo voluptas nulla pariatur Duis finibus sollicitudin libero, sed tempor diam euismod sed. Sed interdum odio a finibus gravida. Aliquam tincidunt nisl nec urna euismod, ullamcorper scelerisque magna vulputate. .</p>
+                    </div> 
+                </div>
+                <div class="col-md-6 w3ls_welcome_right">   
+                    <div class="agileits_w3layouts_welcome_grid">
+                        <img src="{{theme:image_url file='img1.jpg'}}" alt=" " class="img-responsive" />
+                    </div> 
+                </div> 
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+    </div>
     <!-- contact -->
     <div class="contact" id="contact">
         <div class="container"> 
